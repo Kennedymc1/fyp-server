163fcf99dba5463429026e032f9abded1477bd3b
Mundia <fxmundia416@gmail.com>
2022-05-17 07:47:55 +0200

    bug fix

--- resolvers/sensorsResolvers.js
+++ resolvers/sensorsResolvers.js
@@ -1,10 +1,12 @@
+const SensorsModel = require("../models/SensorsModel");
 
 
 module.exports = {
     Query: {
-        
+
 
         sensorData: async (_, __, context) => {
+            return await SensorsModel.findOne()
         }
     }
 };
