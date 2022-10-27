075517bf6212f969e9ca4a0c4f81b7e196a96801
Mundia <fxmundia416@gmail.com>
2022-10-28 00:41:05 +0200

    bug fix

--- raspberrypi/index.js
+++ raspberrypi/index.js
@@ -15,7 +15,7 @@ const devUrl = 'http://localhost:4001'
 const serverUrl = productionUrl
 const socket = io(serverUrl)
 
-app.listen(4000, () => console.log(`server listening on port 3001`))
+app.listen(3001, () => console.log(`server listening on port 3001`))
 
 
 
@@ -35,15 +35,28 @@ fs.watchFile(imagePath, function (current, previous) {
     // socket.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
     fs.readFile(imagePath, (err, data) => {
         if (err) return
-        console.log({ data });
 
-        socket.emit('liveStream', data, (status) => {
-            console.log({ status })
-        })
+        const base64 = _arrayBufferToBase64(data)
+        console.log({ base64 })
+        console.log("feed recieved")
+        const processedData = "data:image/png;base64, " + base64
+
+
+        socket.emit('liveStream', processedData)
 
     });
 
 })
+
+function _arrayBufferToBase64(buffer) {
+    var binary = '';
+    var bytes = new Uint8Array(buffer);
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+}
 
 
 
