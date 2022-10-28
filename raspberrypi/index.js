3a4b04d65a6d6fe5e887bf2c289625f23027f280
Mundia <fxmundia416@gmail.com>
2022-10-28 14:44:31 +0200

    updated socket url

--- raspberrypi/index.js
+++ raspberrypi/index.js
@@ -8,7 +8,7 @@ var path = require('path');
 var spawn = require('child_process').spawn;
 var proc;
 
-const productionUrl = 'https://fyp-socket.herokuapp.com/'
+const productionUrl = 'http://ec2-18-188-141-169.us-east-2.compute.amazonaws.com/'
 
 const devUrl = 'http://localhost:4001'
 
