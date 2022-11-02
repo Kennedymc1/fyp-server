bb18d66e155a97e6ad7d862ca0d7eec7b42b59b9
Mundia <fxmundia416@gmail.com>
2022-11-02 08:52:11 +0200

    bugfix

--- pi/index.js
+++ pi/index.js
@@ -62,9 +62,10 @@ gpio.setup(10, gpio.DIR_IN, gpio.EDGE_BOTH)
 
 
 gpio.on('change', function (channel, value) {
+    console.log('Channel ' + channel + ' value is now ' + value);
     if (channel === 10 && value) {
         if (!cameraRunning) {
-            console.log('Channel ' + channel + ' value is now ' + value);
+            console.log("camera set to true")
             cameraRunning = true
         }
     }
